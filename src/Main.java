8491d9f25408f4cc07a7e42e3d3d7c3d75967ee7
andrei <total.zip.file@gmail.com>
2023-11-23 13:18:25 +0300

    пример использования

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Zoo<Integer, String> zoo = new Zoo<>();
+        zoo.x = 1;
+        zoo.y = "tygy";
+        zoo.print();
+
     }
 }
---
+++ src/Zoo.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+class Zoo<T, V>
+{
+    public T x;
+    public V y;
+
+    public void print(){
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
